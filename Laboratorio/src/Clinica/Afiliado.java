f77ae81be12545c1b8725b8bf0284ce1d338709f
luisgm21 <42901084+luisgm21@users.noreply.github.com>
2018-10-22 12:32:05 -0300

    afiliado-persona
    Responde a lo anterior

--- Laboratorio/src/Clinica/Afiliado.java
+++ Laboratorio/src/Clinica/Afiliado.java
@@ -1,8 +1,13 @@
 
 package Clinica;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
 
 public class Afiliado extends Persona{
-    
-    
+    private int numafiliado;
+    private LocalDateTime fechaultpago;
+    private ArrayList<Persona>familia=new ArrayList();
+    private PagoAfiliado pago;
 }
--- Laboratorio/src/Clinica/PagoAfiliado.java
+++ Laboratorio/src/Clinica/PagoAfiliado.java
@@ -3,5 +3,7 @@ package Clinica;
 
 
 public class PagoAfiliado {
-    
+    private float coutabase;
+    private float coutpfam;
+    private float coutfin;
 }
