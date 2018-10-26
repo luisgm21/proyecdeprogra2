16996bcb88b95c745ab937193a047ff3ce853971
luisgm21 <42901084+luisgm21@users.noreply.github.com>
2018-10-26 10:43:16 -0300

    Pago Afiliado
    Agrege metodo de calculo pago en la clase pago Afiliado.
    Agrege tambien un atributo en la clase Afiliado, numdefam que es el numero de elementos que hay en la lista.

--- Laboratorio/src/Clinica/Afiliado.java
+++ Laboratorio/src/Clinica/Afiliado.java
@@ -9,6 +9,7 @@ public class Afiliado extends Persona{
     private int numafiliado;
     private LocalDateTime fechaultpago;
     private ArrayList<Persona>familia=new ArrayList();
+    private int numdefam;
     private PagoAfiliado pago;
     public Afiliado(){
         
@@ -16,6 +17,9 @@ public class Afiliado extends Persona{
     public Afiliado(String nombre, String dni, String telefono, String direccion, String edad){
         super(nombre,dni,telefono,direccion,edad);
     }
-    
+    public void agregarFamiliar(Persona familiar){
+        familia.add(familiar);
+        numdefam=familia.size();
+    }
 }
 
--- Laboratorio/src/Clinica/PagoAfiliado.java
+++ Laboratorio/src/Clinica/PagoAfiliado.java
@@ -3,20 +3,13 @@ package Clinica;
 
 
 public class PagoAfiliado {
-    private float coutabase=300;
-    private float coutapfam=50;
+    private static final float CUOTABASE=300;
+    private static final float CUOTAPFAM=50;
     private float coutafin;
     
-    public float calculoPago(int tipoafiliado){
-        if(tipoafiliado==1){
-            coutafin=coutabase;
-        }
-        else{
-            if(tipoafiliado==2)
-            {
-                coutafin=coutabase+coutapfam;
-            }
-        }
+    public float calculoPago(int numfam){
+        coutafin=CUOTABASE+CUOTAPFAM*numfam;  
         return coutafin;
     }
 }
+    
