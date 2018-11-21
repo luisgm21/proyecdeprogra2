a4988ed9675ac2e6e58daacf3f46a4fd1fbd1c01
luisgm21 <42901084+luisgm21@users.noreply.github.com>
2018-11-20 23:19:07 -0300

    exit en el boton cancelar

--- Laboratorio/src/Grafica/Emergencia.java
+++ Laboratorio/src/Grafica/Emergencia.java
@@ -269,7 +269,7 @@ public class Emergencia extends javax.swing.JDialog {
     }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
-       
+        this.setVisible(false);
     }//GEN-LAST:event_jButton3ActionPerformed
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
